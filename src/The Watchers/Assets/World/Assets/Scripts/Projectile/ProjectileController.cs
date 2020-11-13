dab6fedbf8f460fe2566c0f9e4495f3aae693041
ADEL\spp109 <martin.magnusson@adelfors.nu>
2020-11-13 10:09:05 +0100

    added particles

---
+++ src/The Watchers/Assets/World/Assets/Scripts/Projectile/ProjectileController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    private bool HasPlayed { get; set; }
+    public ParticleSystem particle;
+    private ParticleSystem explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particle = GetComponentInChildren<ParticleSystem>();
+        explosion = particle.subEmitters.GetSubEmitterSystem(0);
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (HasPlayed) return;
+        HasPlayed = true;
+        particle.Stop();
+        particle.TriggerSubEmitter(0);
+        Debug.Log("TEst");
+    }
+
+}
